bca18b125ae312c466559b49fedacac81c951c61
Natalia Andrea Vera Torres <nataliavera2100@gmail.com>
2022-05-27 20:23:51 -0500

    clase 18
    treeSet

--- app-web/src/main/java/clase18/TreeSetMain.java
+++ app-web/src/main/java/clase18/TreeSetMain.java
@@ -1,9 +1,13 @@
 package clase18;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
+
+
 
 public class TreeSetMain {
 
@@ -37,6 +41,8 @@ public class TreeSetMain {
 		Cheque c4 = new Cheque("1002000", "patagonia", cal.getTime());// IZQ
 
 //		AGREGO UN ORDEN EXTERNO A LA COLLECTION
+		Comparator<Cheque> cmp = new ComparadorCheques();// FUNCIONA PORQUE CHEQUE IMPLEMETA COMPARATOR
+		
 		
 		Collection<Cheque> cheques = new TreeSet<Cheque>(new FechaPagoAsc());
 		cheques.add(c1);
@@ -59,6 +65,11 @@ public class TreeSetMain {
 
 		System.out.println(cheques.isEmpty());
 
+		
+		
+		
+
+		
 	}
 
 }
